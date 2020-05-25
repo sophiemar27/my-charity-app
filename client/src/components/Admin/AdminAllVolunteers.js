229eed4b37f36b975ad18cd20d5f477cc4052b49
Abel <abeltareke@Abels-MacBook-Pro.local>
2020-05-25 12:16:08 -0400

    all volunteers component created on the admin side

---
+++ client/src/components/Admin/AdminAllVolunteers.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+
+     
+export default class AdminAllVolunteers extends Component {
+    state = {
+        volunteers: {
+             name: '',
+             projectName: '',
+            phoneNumber: '',
+            emailAddress: '', 
+        },
+        AllVolunteers: []
+    }
+
+    componentDidMount() {
+        this.getAllVolunteers()
+    }
+ 
+    getAllVolunteers = async () => {
+        try {
+            const res = await axios.get('/api/volunteers')
+            const newState = { ...this.state }
+            newState.AllVolunteers = res.data
+            this.setState(newState)
+        } catch (error) {
+            console.log('Failed to get all volunteer')
+            console.log(error)
+        }
+
+    }
+
+    onDeleteVolunteer = async (volunteerId) => {
+        await axios.delete(`/api/volunteers/${volunteerId}`)
+        this.getAllVolunteers()
+    }
+
+    
+
+    render() {
+        return (
+            <div class='all-volunteers'>
+                {this.state.AllVolunteers.map((volunteers, index) => {
+                    return (
+                        <div key={`jd320kd-${index}`}>
+                            <div> {volunteers.name}</div>
+                            <Link to={`/admin/org/${volunteers._id}`}>
+                            <div>{volunteers.projectName}</div>
+                            </Link>
+                            <div>{volunteers.phoneNumber}</div>
+                            <div>{volunteers.emailAddress}</div>
+                            <button onClick={() => this.onDeleteVolunteer(volunteers._id)}>Delete</button> 
+                            
+                            <button>Edit</button>
+
+                            
+                        </div>
+                    )
+                })}
+            </div>
+        )
+    }
+}
