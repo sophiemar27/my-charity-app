d64e0f1b0814f23a50e86679430d24696072d753
Abel <abeltareke@Abels-MacBook-Pro.local>
2020-05-25 12:27:27 -0400

    route adde to all volunteers page

--- client/src/App.js
+++ client/src/App.js
@@ -12,11 +12,13 @@ import CreateOrganization from './components/Admin/AdminCreateOrg.js'
 import AdminAllOrganizations from './components/Admin/AdminAllOrg.js'
 import AdminSingleOrganization from './components/Admin/AdminSingleOrg.js'
 import CreateVolunteer from './components/Admin/AdminCreateVolunteer.js'
+import AdminAllVolunteers from './components/Admin/AdminAllVolunteers.js'
   //USERS
 import AllProjects from './components/Project/AllProjects.js'
 import SingleProject from './components/Project/SingleProject.js'
 import AllOrganizations from './components/Organization/AllOrganizations.js'
 import SingleOrganization from './components/Organization/SingleOrg.js'
+
 
 
 
@@ -31,11 +33,14 @@ class App extends Component {
         <div>
           <Switch>
 
+            {/* USERS */}
             <Route exact path="/" component={AllProjects}/>
             <Route exact path="/projects/:projectId" component={SingleProject}/>
             <Route exact path="/all_orgs" component={AllOrganizations}/>
             <Route exact path="/organizations/:organizationId" component={SingleOrganization}/>
+           
             
+            {/* ADMI */}
             <Route exact path="/admin" component={CreateProject}/>
             <Route exact path="/admin/projects" component={AdminAllProjects}/>
             <Route exact path="/admin/projects/:projectId" component={AdminSingleProject}/>
@@ -43,6 +48,7 @@ class App extends Component {
             <Route exact path="/admin/organizations" component={AdminAllOrganizations}/>
             <Route exact path="/admin/org/:organizationId" component={AdminSingleOrganization}/>
             <Route exact path="/admin/volunteer" component={CreateVolunteer}/>
+            <Route exact path="/admin/Volunteers" component={AdminAllVolunteers}/>
 
 
 
